3be11afcf3cc4922e50203bd35cee77d50e2ba71
Alvar Maciel <alvarmaciel@gmail.com>
2023-11-18 08:12:24 -0300

    test refactor

--- tests/test_git_bisect.py
+++ tests/test_git_bisect.py
@@ -1,16 +1,23 @@
 import pytest
 
+
+@pytest.fixture
+def number_and_string():
+    a_number = 1
+    a_string = "1"
+    return a_number, a_string
+
 def test_asert_instance():
     a_number = 1
     a_string = "1"
     assert isinstance(a_number, int)
-    assert isinstance(a_number_as_string, str)
+    assert isinstance(a_string, str)
 
 def test_assert_value():
     a_number = 1
     a_string = "1"
     assert a_number == 1
-    assert a_number == "1"
+    assert a_string == "1"
 
 def test_assert_existence():
     a_number = 1
